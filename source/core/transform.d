186fed759c280c5d39cf807a7bc6375238254b6a
Bennet Leff <bennetleff@gmail.com>
2015-03-09 20:39:25 -0500

    made the transform class members editable by properties instead of accessor methods

--- source/core/transform.d
+++ source/core/transform.d
@@ -7,32 +7,32 @@ class Transform
 {
 	this(vec3 pos, vec3 rot, vec3 scale)
 	{
-		this.pos = pos;
-		this.rot = rot;
-		this.scale = scale;
+		this.modPos = pos;
+		this.modRot = rot;
+		this.modScale = scale;
 	}
 	this()
 	{
-		this.pos = vec3(0.0f, 0.0f, 0.0f);
-		this.rot = vec3(0.0f, 0.0f, 0.0f);
-		this.scale = vec3(1.0f, 1.0f, 1.0f);
+		this.modPos = vec3(0.0f, 0.0f, 0.0f);
+		this.modRot = vec3(0.0f, 0.0f, 0.0f);
+		this.modScale = vec3(1.0f, 1.0f, 1.0f);
 	}
 	mat4 getModel () 
 	{
-		return mat4.identity.rotatex(rot.x)
-                            .rotatey(rot.y)
-                            .rotatez(rot.z)
-                            .scale(scale.x, scale.y, scale.z)
-                            .translate(pos.x, pos.y, pos.z);                            
+		return mat4.identity.rotatex(modRot.x)
+                            .rotatey(modRot.y)
+                            .rotatez(modRot.z)
+                            .scale(modScale.x, modScale.y, modScale.z)
+                            .translate(modPos.x, modPos.y, modPos.z);                            
 	}
-	vec3 getPos () { return pos; }
-	vec3 getRot () { return rot; }
-	vec3 getScale () { return scale; }
-	void setPos (vec3 p) { this.pos = p; }
-	void setRot (vec3 r) { this.rot = r; }
-	void setScale (vec3 s) { this.scale = s; }
+	@property ref vec3 pos () { return modPos; }
+	@property ref vec3 rot () { return modRot; }
+	@property ref vec3 scale () { return modScale; }
+	@property ref vec3 pos (vec3 p) { return this.modPos = p; }
+	@property ref vec3 rot (vec3 r) { return this.modRot = r; }
+	@property ref vec3 scale (vec3 s) { return this.modScale = s; }
 private:
-	vec3 pos;
-	vec3 rot;
-	vec3 scale;
+	vec3 modPos;
+	vec3 modRot;
+	vec3 modScale;
 }
